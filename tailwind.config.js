cc7e43832653aa97969d68c9b9d9d182d63ba8aa
zepang <ummlq4@gmail.com>
2020-12-13 09:17:49 +0800

    添加tailwindcss purgecss 配置

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  purge: [],
+  purge: {
+    content: ['./pages/**/*.js', './pages/*.js', './components/*.js']
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
