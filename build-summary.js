740be714fbd0f2cb941f8edf7a1b8749e4c330da
zepang <ummlq4@gmail.com>
2020-12-30 13:13:16 +0800

    更新 build-summary.js 逻辑，开发模式下支持单markdown文件修改更新

--- build-summary.js
+++ build-summary.js
@@ -1,26 +1,35 @@
 const chokidar = require('chokidar')
 const path = require('path')
 const fs = require('fs-extra')
-const { getAllMdPost } = require('./utils/post')
+const { getAllMdPost, getMdPost } = require('./utils/post')
 const debounce = require('debounce')
+const summaryFile = path.resolve(__dirname, 'summary.json')
 
 async function updateSummary () {
-  const summayFile = path.resolve(__dirname, 'summary.json')
-  await fs.ensureFile(summayFile)
+  await fs.ensureFile(summaryFile)
   let posts = await getAllMdPost()
   let jsonStr = JSON.stringify(posts, null, 2)
-  fs.writeFileSync(summayFile, jsonStr)
+  fs.writeFileSync(summaryFile, jsonStr)
 }
 
-const handleFileChange = debounce(async () => {
-  await updateSummary()
+const handleFileChange = debounce(async (name) => {
+  await fs.ensureFile(summaryFile)
+  const summary = require(summaryFile)
+  const newPost = await getMdPost(name)
+  const oldPost = summary.find(post => post.name === name)
+  Object.assign(oldPost, newPost)
+  const jsonStr = JSON.stringify(summary, null, 2)
+  fs.writeFileSync(summaryFile, jsonStr)
 }, 250)
 
 if (process.env.npm_config_development) {
-  chokidar.watch(path.resolve(__dirname, 'posts')).on('all', async (event, filePath) => {
+  chokidar.watch(path.resolve(__dirname, 'posts'), {
+    ignoreInitial: true
+  }).on('all', async (event, filePath) => {
     console.log(`${event} => `, filePath)
-    handleFileChange()
+    const name = path.basename(filePath, path.extname(filePath))
+    handleFileChange(name)
   })
-} else {
-  updateSummary()
-}
+} 
+
+updateSummary()
