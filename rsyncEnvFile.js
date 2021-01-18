b318b5c032c97670c7987ea72c31716e9d1c899b
zepang <ummlq4@gmail.com>
2021-01-18 21:26:57 +0800

    fix语法问题

--- rsyncEnvFile.js
+++ rsyncEnvFile.js
@@ -1,8 +1,7 @@
-const execa = require('execa')
-const path = require('path')
-const serverConfig = require('./server.config')
-
 (async function startDeploy () {
+  const execa = require('execa')
+  const serverConfig = require('./server.config')
+  
   let { stdout } = await execa('rsync', ['-r', '.env.production.local', `${serverConfig.user}@${serverConfig.host}:${serverConfig.path}`, '--progress', '-vv'])
   console.log(stdout)
 })()
