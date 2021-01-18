c94b69383cac16fdc8b93c0d501eed9dcb8ec27d
zepang <ummlq4@gmail.com>
2021-01-18 21:33:02 +0800

    更新同步文件的脚本

--- rsyncEnvFile.js
+++ rsyncEnvFile.js
@@ -1,7 +1,7 @@
 (async function startDeploy () {
   const execa = require('execa')
   const serverConfig = require('./server.config')
-  
-  let { stdout } = await execa('rsync', ['-r', '.env.production.local', `${serverConfig.user}@${serverConfig.host}:${serverConfig.path}`, '--progress', '-vv'])
+
+  let { stdout } = await execa('rsync', ['-r', '.env.production.local', 'server.config.js', `${serverConfig.user}@${serverConfig.host}:${serverConfig.path}`, '--progress', '-vv'])
   console.log(stdout)
 })()
