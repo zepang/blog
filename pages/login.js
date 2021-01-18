991942626fed570e278bdbeffdf80dd07d829432
zepang <ummlq4@gmail.com>
2021-01-18 21:55:10 +0800

    clientId取环境变量

--- pages/login.js
+++ pages/login.js
@@ -1,8 +1,8 @@
 import Link from 'next/link'
 
-export default function Login ({ siteOrigin }) {
+export default function Login ({ siteOrigin, clientId }) {
   return (
-     <Link href={`https://github.com/login/oauth/authorize?client_id=1f7c31836146cca48edc&redirect_uri=${siteOrigin}/api/auth/redirect`}>
+     <Link href={`https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${siteOrigin}/api/auth/redirect`}>
       <a>
         <button className={`block-inline text-white bg-green-400 p-4 hover:bg-green-500`}>
           github authorcation
@@ -14,10 +14,12 @@ export default function Login ({ siteOrigin }) {
 
 Login.getInitialProps = function () {
   const siteOrigin = process.env.SITE_ORIGIN
+  const clientId = process.env.GITHUB_CLIENT_ID
   console.log(process.env.SITE_ORIGIN)
   console.log(process.env.GITHUB_CLIENT_ID)
   console.log(process.env.GITHUB_CLIENT_SECRET)
   return {
-    siteOrigin
+    siteOrigin,
+    clientId
   }
 }
