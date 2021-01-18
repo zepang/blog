dc952832f38f80be4dea1ba7ff7b23cb3cea3dd5
zepang <ummlq4@gmail.com>
2021-01-18 21:50:13 +0800

    fix

--- pages/login.js
+++ pages/login.js
@@ -14,6 +14,7 @@ export default function Login ({ siteOrigin }) {
 
 Login.getInitialProps = function () {
   const siteOrigin = process.env.SITE_ORIGIN
+  console.log(process.env.SITE_ORIGIN)
   return {
     siteOrigin
   }
--- rsyncEnvFile.js
+++ rsyncEnvFile.js
@@ -2,6 +2,6 @@
   const execa = require('execa')
   const serverConfig = require('./server.config')
 
-  let { stdout } = await execa('rsync', ['-r', '.env.production.local', 'server.config.js', `${serverConfig.user}@${serverConfig.host}:${serverConfig.path}`, '--progress', '-vv'])
+  let { stdout } = await execa('rsync', ['-r', '.env.production.local', 'server.config.js', `${serverConfig.user}@${serverConfig.host}:${serverConfig.path}/current`, '--progress', '-vv'])
   console.log(stdout)
 })()
